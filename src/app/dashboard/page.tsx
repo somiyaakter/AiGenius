8d06b3a22d4263386ed9c121c087552b8630c3d2
somiya <somiyaakterrimo2021@gmail.com>
2025-10-17 20:06:08 +0600

    feat(dashboard): fixed dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,87 @@
 "use client";
+import {
+  ImageIcon,
+  MessageSquare,
+  VideoIcon,
+  Music,
+  Code,
+  ArrowRight,
+} from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-400",
+    bgColor: "bg-violet-400/10",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-400",
+    bgColor: "bg-pink-400/10",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-amber-400",
+    bgColor: "bg-amber-400/10",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-emerald-400",
+    bgColor: "bg-emerald-400/10",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    href: "/code",
+    color: "text-blue-400",
+    bgColor: "bg-blue-400/10",
+  },
+];
 
 export default function Dashboard() {
+  const router = useRouter();
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      jjj
+    <div>
+      <div className="mb-8 space-y-4">
+        <h1 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h1>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black/5 hover:shadow-md cursor-pointer transition"
+          >
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-x-4">
+                <div className={cn("p-2 rounded-md w-fit", tool.bgColor)}>
+                  <tool.icon className={cn("h-8 w-8", tool.color)} />
+                </div>
+                <div className="font-semibold">{tool.label}</div>
+              </div>
+              <ArrowRight className="h-5 w-5" />
+            </div>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
